21b75d3dae5e8f872ab76edbf5d920121ad19e01
Andres Navarro <contact@andresnav.com>
2024-04-30 08:46:21 +0200

    dev: ml model setup

---
+++ ml/main.py
@@ -0,0 +1,25 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+# Check if the webcam is opened correctly
+if not cap.isOpened():
+    raise IOError("Cannot open webcam")
+
+
+while True:
+    ret, frame = cap.read()
+
+    # Check if frame is successfully read
+    if not ret:
+        print("Failed to grab frame")
+        break
+
+    frame = cv2.resize(frame, None, fx=0.25, fy=0.25, interpolation=cv2.INTER_AREA)
+
+    cv2.imshow("Input", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
